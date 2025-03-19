de068538561881256a4dbbd8f7c81edc78974aa8
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-03-19 10:48:56 -0400

    src/main.rs
    get_ssh_key
    
    apply cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 mod client;
 mod server;
 
-use std::{env, io};
+use crate::client::run_client;
+use crate::server::run_server;
+use clap::{Parser, Subcommand};
+use libp2p::futures::StreamExt;
+use libp2p::swarm::SwarmEvent;
+use libp2p::{Multiaddr, PeerId, Swarm};
+use libp2p_proxy::client::ProxyClient;
+use libp2p_proxy::server::ProxyServer;
+use log::{debug, error};
+use rand::SeedableRng;
+use ssh_key::private::{KeypairData, RsaKeypair, RsaPrivateKey};
+use ssh_key::public::{Ed25519PublicKey, RsaPublicKey};
+use ssh_key::{Algorithm, PrivateKey, PublicKey};
 use std::error::Error;
 use std::fs::read_to_string;
 use std::io::Write;
 use std::net::SocketAddr;
-use std::path::PathBuf;
-use libp2p::{Multiaddr, PeerId, Swarm};
-use libp2p::futures::StreamExt;
-use libp2p::swarm::SwarmEvent;
-use rand::SeedableRng;
-use ssh_key::private::{KeypairData, RsaKeypair, RsaPrivateKey};
-use ssh_key::{Algorithm, PrivateKey, PublicKey};
-use libp2p_proxy::client::ProxyClient;
-use libp2p_proxy::server::ProxyServer;
-use clap::{Parser, Subcommand};
-use log::{debug, error};
-use ssh_key::public::{Ed25519PublicKey, RsaPublicKey};
-use crate::client::run_client;
-use crate::server::run_server;
+use std::path::{Path, PathBuf};
+use std::{env, fs, io};
 
 const BOOTNODES: [&str; 4] = [
     "QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
@@ -48,7 +48,6 @@ enum Commands {
 
     /// Start proxy client
     Client {
-
         /// Address to start proxy
         local_addr: SocketAddr,
 
@@ -56,24 +55,57 @@ enum Commands {
         remote_addr: SocketAddr,
 
         /// Server peer to connect
-        peer_id: PeerId
-    }
+        peer_id: PeerId,
+    },
+}
+
+fn get_ssh_key() -> Result<String, Box<dyn std::error::Error>> {
+    let home_dir = std::env::var("HOME")?;
+    let ssh_key_path = Path::new(&home_dir).join(".ssh").join("id_rsa");
+
+    let ssh_key = fs::read_to_string(ssh_key_path)?;
+    Ok(ssh_key)
 }
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init();
+
+    // match get_ssh_key() {
+    //     Ok(key) => {
+    //         //println!("{}", key);
+    //     }
+    //     Err(e) => {
+    //         eprintln!("Error getting SSH key: {}", e);
+    //     }
+    // }
+
     let cli = Cli::parse();
 
     match cli.command {
         Commands::Server => {
-            let key_path: PathBuf = cli.key;
-            let key = read_to_string(key_path)?;
-            let key = PublicKey::from_openssh(key.as_str())?;
-            run_server(key).await?;
+            if cli.key.exists() {
+                let key_path: PathBuf = cli.key;
+                let key = read_to_string(key_path)?;
+                let key = PublicKey::from_openssh(key.as_str())?;
+                run_server(key).await?;
+            } else {
+                match get_ssh_key() {
+                    Ok(key) => {
+                        println!("{}", key);
+                        let key = PublicKey::from_openssh(key.as_str())?;
+                        run_server(key).await?;
+                    }
+                    Err(e) => {
+                        eprintln!("Error getting SSH key: {}", e);
+                    }
+                }
+            }
         }
         Commands::Client {
-            local_addr, remote_addr, peer_id
+            local_addr,
+            remote_addr,
+            peer_id,
         } => {
             let key_path: PathBuf = cli.key;
             let key = read_to_string(key_path)?;
@@ -89,7 +121,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     error!("Cannot decrypt key: {err}");
                 }
                 res?
-            } else { key };
+            } else {
+                key
+            };
 
             run_client(key, local_addr, peer_id, remote_addr).await?;
         }
